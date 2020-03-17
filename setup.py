d0f360317dab2c6e24a2d300094dffc464623b7e
johnmrudolph <contact@johnmrudolph.com>
2020-03-16 20:50:08 -0700

    added setup.py with requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 # setup.py
 # https://blog.godatadriven.com/setup-py
+# pip install e .
 from setuptools import setup, find_packages
 
 setup(
-    name='py-enduse',
-    packages=find_packages()
+    name = 'py-enduse',
+    version = '0.1.0',
+    author = 'John Rudolph',
+    author_email = 'contact@johnmrudolph.com',
+    description = ('Python implementation of stock turnover based end-use model'),
+    packages = find_packages(),
+    install_requires = [
+        'numpy',
+        'numba',
+        'pytest',
+        'jupyter',
+    ]
 )
